44e14169aabae90c6f73a3eb2bd6163bd2d407b2
David Romero <ale.romhdz@gmail.com>
2024-12-02 10:17:12 -0600

    Error handling + draw component

--- secret-santa-app/src/app/participant-input/participant-input.component.ts
+++ secret-santa-app/src/app/participant-input/participant-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
 
@@ -11,27 +11,47 @@ import { FormsModule } from '@angular/forms'
 })
 
 export class ParticipantInputComponent {
+  @Output() listFinalized = new EventEmitter<
+   { name: string; lastNameLetter: string; nickname: string }[]
+   >();
+
   name: string = '';
   lastNameLetter: string = '';
   nickname: string = '';
   participants: { name: string; lastNameLetter: string; nickname: string; } [] = [];
+  duplicateParticipantError: boolean = false;
 
   addParticipant() {
     if (this.name && this.lastNameLetter && this.nickname) {
-      this.participants.push({
-        name: this.name,
-        lastNameLetter: this.lastNameLetter,
-        nickname: this.nickname
-      });
-      this.name = '';
-      this.lastNameLetter = '';
-      this.nickname = '';
+      const isDuplicate = this.participants.some(
+        (participant) =>
+          participant.name === this.name &&
+          participant.lastNameLetter === this.lastNameLetter &&
+          participant.nickname === this.nickname
+      );
+      if (isDuplicate) {
+        this.duplicateParticipantError = true;
+      } else {
+        this.participants.push({
+          name: this.name,
+          lastNameLetter: this.lastNameLetter,
+          nickname: this.nickname
+        });
+
+        this.name ='';
+        this.lastNameLetter = '';
+        this.nickname = '';
+        this.duplicateParticipantError = false;
+      }
     } else {
-      alert('Please fill in all fields!');
+      alert('Please fill in all fields!')
     }
   }
+
+  removeParticipant(index: number) {
+    this.participants.splice(index, 1)
+   }
   finishList() {
-    console.log('Final Participant List:', this.participants);
-    alert('Participant list is finalized!');
+    this.listFinalized.emit(this.participants);
   }
-  }
+}
